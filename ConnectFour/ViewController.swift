1b9e3da3bbbfb76e1dbe309fddb031d951bf3a89
Evandro Harrison Hoffmann <evandro@itsdayoff.com>
2018-04-14 09:23:32 +0800

    Initial Commit

---
+++ ConnectFour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConnectFour
+//
+//  Created by Evandro Harrison Hoffmann on 4/14/18.
+//  Copyright © 2018 It's Day Off. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
